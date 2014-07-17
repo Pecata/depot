58ff700fad6bcfe42d4d87074c3181a7fd2fc040
Petko <petko_yanakievv@abv.bg>
2014-07-17 17:20:07 +0300

    Add store controller

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,4 +2,13 @@ class StoreController < ApplicationController
   def index
     @products = Product.find_product_for_sale
   end
+
+
+  private
+
+
+  def find_cart
+    session[:cart] ||= Cart.new
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717081016) do
+ActiveRecord::Schema.define(version: 20140717124925) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -21,5 +21,15 @@ ActiveRecord::Schema.define(version: 20140717081016) do
     t.datetime "updated_at"
     t.decimal  "price",       precision: 8, scale: 2, default: 0.0
   end
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
 end
